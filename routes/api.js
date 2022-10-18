e4e740744c6b9450e9c9399e9bf8939588ae7d1c
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-10-17 22:41:44 -0400

    Delete index.html and redirect home page to polls page

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/api', (req, res, next) => {
+// poll routes
+router.get('/', (req, res) => {
   res.redirect('/api/polls');
 });
 
-// poll routes
 router.get('/polls', (req, res) => {
   res.send('GET POLLS');
+});
+
+router.post('/polls', (req, res) => {
+  res.send('POST POLLS');
+});
+
+router.delete('/polls/:pollId', (req, res) => {
+  res.send('Delete Poll');
 });
 
 module.exports = router;
