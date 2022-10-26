77c5a8b80791a357e8316f2e9718f2a6ab11af6f
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2022-10-26 15:08:37 -0400

    Add the functions for populating our testing database in seed.js

--- seed.js
+++ seed.js
@@ -1,17 +1,16 @@
 import { faker } from '@faker-js/faker';
+
 const Poll = require('./models/poll');
 const User = require('./models/user');
 
-const users = [];
 const polls = [];
+const users = [];
 
 const shuffleArray = (arr) => {
   const array = [...arr];
   for (let i = array.length - 1; i > 0; i--) {
-    const j = (Math.floor(Math.random() * (i + 1))[(array[i], array[j])] = [
-      array[j],
-      array[i],
-    ]);
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
 };
@@ -21,6 +20,7 @@ const generateUser = () => {
     username: faker.internet.username(),
     password: faker.internet.password(),
     profilePicUrl: faker.image.imageUrl(),
+    polls: [],
     friends: [],
     friendRequests: [],
   });
@@ -48,7 +48,7 @@ const generateFriendRequests = () => {
       item._id != user._id && !user.friends.includes(item);
     });
     const shuffledRequests = shuffleArray(potentialFriendReqs);
-    const randUsers = shuffledUsers.slice(0, 1);
+    const randUsers = shuffledRequests.slice(0, 1);
     user.friendRequests = randUsers.map((user) => user._id);
   });
 };
@@ -64,5 +64,56 @@ const generatePoll = (user) => {
     timestamp: faker.date.past(2),
   });
   polls.push(poll);
-  user.poll;
+  user.polls.push(poll._id);
 };
+
+const addPolls = () => {
+  users.forEach((user) => {
+    for (let i = 0; i < Math.floor(Math.random() * 6); i++) {
+      generatePoll(user);
+    }
+  });
+};
+
+const addVotes = () => {
+  polls.forEach((poll) => {
+    poll.author.friends.forEach((friend) => {
+      if (Math.random() > 0.5) {
+        poll.answers[1].vote.push(friend._id);
+      } else poll.answers[0].vote.push(friend._id);
+    });
+  });
+};
+
+const seedDB = () => {
+  for (let i = 0; i < 100; i++) {
+    generateUser();
+  }
+
+  generateFriendRequests();
+  generateFriends();
+  addPolls();
+  addVotes();
+
+  users.forEach(async (user) => {
+    try {
+      await user.save();
+    } catch (err) {
+      return err;
+    }
+  });
+
+  polls.forEach(async (poll) => {
+    try {
+      await poll.save();
+    } catch (err) {
+      return err;
+    }
+  });
+  console.log({ users, posts });
+  return { users, polls };
+};
+
+seedDB();
+
+module.exports = seedDB;
